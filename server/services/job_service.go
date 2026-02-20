dcd687f687947306c4d0a3b84f3047acb8c39fdd
KANU <kanu@getkanu.com>
2026-02-20 07:22:52 +0000

    feat: Update services/job_service.go for DynamoDB operations (fc7c8a)

--- server/services/job_service.go
+++ server/services/job_service.go
@@ -4,58 +4,121 @@ import (
 	"context"
 	"time"
 
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/bson/primitive"
-	"go.mongodb.org/mongo-driver/mongo/options"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
+	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/expression"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
 
 	"server/models"
 )
 
+// ListJobs retrieves all jobs for a user from the jobs table.
+// Jobs table has userId as partition key and id as sort key.
+// Results are returned and can be sorted by frontend (DynamoDB doesn't easily sort by non-key attributes).
 func ListJobs(ctx context.Context, userID string) ([]models.Job, error) {
-	filter := bson.M{"userId": userID}
-	opts := options.Find().SetSort(bson.D{{Key: "updatedAt", Value: -1}})
+	client := GetDynamoClient()
+	tableName := GetJobsTableName()
 
-	cur, err := JobsCollection().Find(ctx, filter, opts)
+	keyCond := expression.Key("userId").Equal(expression.Value(userID))
+	expr, err := expression.NewBuilder().WithKeyCondition(keyCond).Build()
 	if err != nil {
 		return nil, err
 	}
-	defer cur.Close(ctx)
 
-	var jobs []models.Job
-	if err := cur.All(ctx, &jobs); err != nil {
+	result, err := client.Query(ctx, &dynamodb.QueryInput{
+		TableName:                 aws.String(tableName),
+		KeyConditionExpression:    expr.KeyCondition(),
+		ExpressionAttributeNames:  expr.Names(),
+		ExpressionAttributeValues: expr.Values(),
+	})
+	if err != nil {
 		return nil, err
 	}
+
+	var jobs []models.Job
+	err = attributevalue.UnmarshalListOfMaps(result.Items, &jobs)
+	if err != nil {
+		return nil, err
+	}
+
 	return jobs, nil
 }
 
+// InsertJob inserts a new job into the jobs table.
 func InsertJob(ctx context.Context, j models.Job) error {
-	_, err := JobsCollection().InsertOne(ctx, j)
-	return err
-}
+	client := GetDynamoClient()
+	tableName := GetJobsTableName()
 
-func UpdateJob(ctx context.Context, jobIDHex string, userID string, update bson.M) error {
-	oid, err := primitive.ObjectIDFromHex(jobIDHex)
+	item, err := attributevalue.MarshalMap(j)
 	if err != nil {
 		return err
 	}
 
-	_, err = JobsCollection().UpdateOne(ctx,
-		bson.M{"_id": oid, "userId": userID},
-		bson.M{"$set": update},
-	)
+	_, err = client.PutItem(ctx, &dynamodb.PutItemInput{
+		TableName: aws.String(tableName),
+		Item:      item,
+	})
+
 	return err
 }
 
-func DeleteJob(ctx context.Context, jobIDHex string, userID string) error {
-	oid, err := primitive.ObjectIDFromHex(jobIDHex)
+// UpdateJob updates a job in the jobs table.
+// The update parameter is a map of field names to new values.
+// Jobs table has userId as partition key and id as sort key.
+func UpdateJob(ctx context.Context, jobID string, userID string, update map[string]interface{}) error {
+	client := GetDynamoClient()
+	tableName := GetJobsTableName()
+
+	var updateBuilder expression.UpdateBuilder
+	first := true
+	for key, value := range update {
+		if first {
+			updateBuilder = expression.Set(expression.Name(key), expression.Value(value))
+			first = false
+		} else {
+			updateBuilder = updateBuilder.Set(expression.Name(key), expression.Value(value))
+		}
+	}
+
+	expr, err := expression.NewBuilder().WithUpdate(updateBuilder).Build()
 	if err != nil {
 		return err
 	}
 
-	_, err = JobsCollection().DeleteOne(ctx, bson.M{"_id": oid, "userId": userID})
+	_, err = client.UpdateItem(ctx, &dynamodb.UpdateItemInput{
+		TableName: aws.String(tableName),
+		Key: map[string]types.AttributeValue{
+			"userId": &types.AttributeValueMemberS{Value: userID},
+			"id":     &types.AttributeValueMemberS{Value: jobID},
+		},
+		UpdateExpression:          expr.Update(),
+		ExpressionAttributeNames:  expr.Names(),
+		ExpressionAttributeValues: expr.Values(),
+	})
+
 	return err
 }
 
-func NowUTC() time.Time {
-	return time.Now().UTC()
+// DeleteJob deletes a job from the jobs table.
+// Jobs table has userId as partition key and id as sort key.
+func DeleteJob(ctx context.Context, jobID string, userID string) error {
+	client := GetDynamoClient()
+	tableName := GetJobsTableName()
+
+	_, err := client.DeleteItem(ctx, &dynamodb.DeleteItemInput{
+		TableName: aws.String(tableName),
+		Key: map[string]types.AttributeValue{
+			"userId": &types.AttributeValueMemberS{Value: userID},
+			"id":     &types.AttributeValueMemberS{Value: jobID},
+		},
+	})
+
+	return err
+}
+
+// NowUTC returns the current UTC time as an RFC3339 formatted string.
+// This is used for DynamoDB timestamp fields which are stored as strings.
+func NowUTC() string {
+	return time.Now().UTC().Format(time.RFC3339)
 }
