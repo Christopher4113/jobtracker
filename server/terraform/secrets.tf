9954a383b523b228791f8ca0e7acf40748ae9ed4
KANU <kanu@getkanu.com>
2026-02-20 07:06:07 +0000

    feat: Update secrets.tf to remove MONGO_URI, keeping only JWT_SECRET (96a6b7)

--- server/terraform/secrets.tf
+++ server/terraform/secrets.tf
@@ -12,7 +12,6 @@ resource "aws_secretsmanager_secret" "app_secrets" {
 resource "aws_secretsmanager_secret_version" "app_secrets" {
   secret_id = aws_secretsmanager_secret.app_secrets.id
   secret_string = jsonencode({
-    MONGO_URI  = "mongodb+srv://christopherl4n_db_user:rHy9TfrKNPIkDK5m@cluster0.jb4hydn.mongodb.net/?appName=Cluster0"
     JWT_SECRET = "super_secret_change_me"
   })
 }
